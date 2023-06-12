dcf7c234ea65a51c283e3fc7bf0d3368f4a9481e
tarakarama <tarakarama078@gmail.com>
2023-06-12 21:59:05 +0530

    grid page changes

--- src/HomePage/correspondent.jsx
+++ src/HomePage/correspondent.jsx
@@ -6,22 +6,38 @@ import image from "../images/F29A9496.JPG";
 
 function Correspondent() {
     return (
- <Container style={{ backgroundImage:`url(${image})` }}>
-            <Row>
-                <Col>
-                    <img src='./images/Madam.jpg' className='madam' />
-                </Col>
-                <Col>
-                    <p className='text1'>
+//  <Container >
+//             <Row>
+//                 <Col>
+//                     <img src='./images/Madam.jpg' className='madam' />
+//                 </Col>
+//                 <Col className='correspondenttext'>
+//                     <p className='text1'>
+//                        Mrs. SOUMINI REDDY a simple and benign person,
+//                         did her schooling in Bihar and collegiate education at Bangalore.
+//                         She has been the correspondent of B.V.REDDY SCHOOL from 1 st December 1986.
+//                         She is credited as the most significant person who has made the founder's vision, a reality.
+//                         She has utilised all her energy into moulding and shaping an established school of repute.
+//                     </p>
+//                 </Col>
+//             </Row>
+//   </Container>
+  <div >
+    <div class='row'>
+        <div class='col-6'>
+                 <img src='./images/Madam.jpg' className='madam' />
+         </div>
+         <div class='col-6'>
+         <p className='text1'>
                        Mrs. SOUMINI REDDY a simple and benign person,
                         did her schooling in Bihar and collegiate education at Bangalore.
                         She has been the correspondent of B.V.REDDY SCHOOL from 1 st December 1986.
                         She is credited as the most significant person who has made the founder's vision, a reality.
                         She has utilised all her energy into moulding and shaping an established school of repute.
                     </p>
-                </Col>
-            </Row>
-            </Container>
+         </div>
+    </div>
+  </div>
     );
 }
 
--- src/HomePage/gridPage.jsx
+++ src/HomePage/gridPage.jsx
@@ -2,67 +2,227 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Carousel from 'react-bootstrap/Carousel';
+import { useState } from 'react';
+ function GridPage(){
+  const [index, setIndex] = useState(0);
 
-function GridPage(){
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);}
+
     return (
-    <Container>
-    <Row>
-    <Col>
-     <Row>
-     <Col className='rowitems'><h3>Experienced Teachers</h3>
-      <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
-        </Col>
-        <Col>
-        <img src='./images/Teacher.jpg' alt='girls' className='gridPageImage'/></Col>
+  //   <Container>
+  //   <Row>
+  //   <Col>
+  //    <Row>
+  //    <Col className='rowitems'><h3>Experienced Teachers</h3>
+  //     <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
+  //       </Col>
+  //       <Col>
+  //       <img src='./images/Teacher.jpg' alt='girls' className='gridPageImage'/></Col>
        
-     </Row>
-     <Row>
+  //    </Row>
+  //    <Row>
        
-        <Col className='rowitems'><h3>A/C Residential campus</h3>
-      <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
-        </Col>
-        <Col>
-        <img src='./images/AC.png' alt='girls' className='gridPageImage'/>
-        </Col>
-     </Row>
-     <Row>
+  //       <Col className='rowitems'><h3>A/C Residential campus</h3>
+  //     <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
+  //       </Col>
+  //       <Col>
+  //       <img src='./images/AC.png' alt='girls' className='gridPageImage'/>
+  //       </Col>
+  //    </Row>
+  //    <Row>
        
-        <Col className='rowitems'><h3>Delicious food</h3>
-      <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
-        </Col>
-        <Col>
-        <img src='./images/Teacher.jpg' alt='girls' className='gridPageImage'/>
-        </Col>
-     </Row>
-     </Col>
-      <Col xs={6}>  <img src='./images/C3750T01.jpg' alt='girls' className='body'/></Col>
-     <Col>
-     <Row>
-        <Col>
-        <img src='./images/Teacher.jpg' alt='girls' className='gridPageImage'/></Col>
+  //       <Col className='rowitems'><h3>Delicious food</h3>
+  //     <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
+  //       </Col>
+  //       <Col>
+  //       <img src='./images/Teacher.jpg' alt='girls' className='gridPageImage'/>
+  //       </Col>
+  //    </Row>
+  //    </Col>
+      
+  //     <Col class='col-4' > 
+  //     <Carousel activeIndex={index} onSelect={handleSelect} variant='transperent'  >
+  //     <Carousel.Item >
+  //       <img
+  //         className="d-block w-100 h-30"
+  //         src="./images/C3750T01.jpg"
+  //         alt="First slide"
+          
+  //       />
+  //     </Carousel.Item>
+  //     <Carousel.Item>
+  //       <img
+  //         className="d-block w-100"
+  //         src="./images/F29A9495.jpg"
+  //         alt="Second slide" />
+  //     </Carousel.Item>
+  //     <Carousel.Item>
+  //       <img
+  //         className="d-block w-100"
+  //         src="./images/C3750T01.jpg"
+  //         alt="Third slide"
+  //       />
+
+  //     </Carousel.Item>
+  //   </Carousel>    
+  //       {/* <img src='./images/C3750T01.jpg' alt='girls' className='body'/>  */}
+  //      </Col>
+    
+  //    <Col>
+  //    <Row>
+  //       <Col>
+  //       <img src='./images/Teacher.jpg' alt='girls' className='gridPageImage'/></Col>
+  //       <Col className='rowitems'><h3>Experienced Teachers</h3>
+  //     <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
+  //       </Col>
+  //    </Row>
+  //    <Row>
+  //       <Col>
+  //       <img src='./images/AC.png' alt='girls' className='gridPageImage'/>
+  //       </Col>
+  //       <Col className='rowitems'><h3>A/C Residential campus</h3>
+  //     <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
+  //       </Col>
+  //    </Row>
+  //    <Row>
+  //       <Col>
+  //       <img src='./images/Teacher.jpg' alt='girls' className='gridPageImage'/>
+  //       </Col>
+  //       <Col className='rowitems'><h3>Delicious food</h3>
+  //     <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
+  //       </Col>
+  //    </Row>
+  //    </Col>
+  //   </Row>
+  // </Container>
+  <div class='container'>
+    <div class='row'>
+      <div class='col'>
+      <Row>
         <Col className='rowitems'><h3>Experienced Teachers</h3>
-      <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
+          <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
         </Col>
-     </Row>
-     <Row>
-        <Col>
-        <img src='./images/AC.png' alt='girls' className='gridPageImage'/>
-        </Col>
-        <Col className='rowitems'><h3>A/C Residential campus</h3>
-      <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
-        </Col>
-     </Row>
-     <Row>
-        <Col>
-        <img src='./images/Teacher.jpg' alt='girls' className='body'/>
-        </Col>
+         <Col className='imagealign'><img src='./images/Teacher.jpg' alt='girls' className='gridPageImage'/></Col>
+      </Row>
+      </div>
+      <div class='col-6'>
+      <Carousel   >
+        <Carousel.Item >
+          <img
+            className="d-block w-100 "
+            src="./images/AB6I1043.jpg"
+            alt="First slide"
+      
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="./images/F29A9495.jpg"
+            alt="Second slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="./images/AB6I1043.jpg"
+            alt="Third slide"/>
+        </Carousel.Item>
+      </Carousel>    
+      </div>
+      <div class='col'>
+      <Row>
         <Col className='rowitems'><h3>Delicious food</h3>
       <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
         </Col>
+        <Col className='imagealign'> <img src='./images/Teacher.jpg' alt='girls' className='gridPageImage'/> </Col>
      </Row>
-     </Col>
-    </Row>
-  </Container>
+      </div>
+    </div>
+    <div class='row'>
+      <div class='col'>
+      <Row>
+       
+            <Col className='rowitems'><h3>A/C Residential campus</h3>
+             <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
+            </Col>
+            <Col className='imagealign'> <img src='./images/AC.png' alt='girls' className='gridPageImage'/> </Col>
+         </Row>
+      </div>
+      <div class='col-4'>
+      <img
+            className="col4image"
+            src="./images/C3750T01.jpg"
+            alt="First slide"
+            
+          />
+      </div>
+      <div class='col-2' >
+      {/* <img
+            className="col2image"
+            src="./images/C3750T01.jpg"
+            alt="First slide"
+            
+          /> */}
+          <div className='divbox'>
+          <h4>Dream</h4>
+          <h4>Belive</h4>
+          <h4>Achieve</h4>
+          </div>
+      </div>
+      <div class='col'>
+      <Row>
+        <Col className='rowitems'><h3>Delicious food</h3>
+      <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
+        </Col>
+        <Col className='imagealign'> <img src='./images/AC.png' alt='girls' className='gridPageImage'/> </Col>
+     </Row>
+      </div>
+    </div>
+    <div class='row'>
+      <div class='col'>
+      <Row>
+        <Col className='rowitems'><h3>Delicious food</h3>
+      <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
+        </Col>
+        <Col className='imagealign'> <img src='./images/AC.png' alt='girls' className='gridPageImage'/> </Col>
+     </Row>
+
+      </div>
+      <div class='col-3'>
+      <img
+            className="d-block w-100"
+            src="./images/C3750T01.jpg"
+            alt="Third slide"/>
+      </div>
+      <div class='col-3'>
+      <Carousel >
+        
+        <Carousel.Item>
+          <img
+            className="d-block w-100 "
+            src="./images/F29A9495.jpg"
+            alt="Second slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="./images/C3750T01.jpg"
+            alt="Third slide"/>
+        </Carousel.Item>
+      </Carousel>    
+      </div>
+      <div class='col'>
+      <Row>
+        <Col className='rowitems'><h3>Delicious food</h3>
+      <p className='text'>soijwej welkjwej lkjkjowej oijnksndksnd</p>
+        </Col>
+        <Col className='imagealign'> <img src='./images/AC.png' alt='girls' className='gridPageImage'/> </Col>
+     </Row>
+      </div>
+    </div>
+  </div>
     )
 }
 export default GridPage
